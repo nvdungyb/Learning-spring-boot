32cec79c49d559c61ba9544f7babb222254efec2
NvDzung <dung3032003md@gmail.com>
2024-03-20 20:43:37 +0700

    @Value - ApplicationConfig

--- src/main/java/com/dzung/springboot/configurationbean/App.java
+++ src/main/java/com/dzung/springboot/configurationbean/App.java
@@ -4,13 +4,14 @@ import com.dzung.springboot.configurationbean.database.MysqlConnector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import com.dzung.springboot.configurationbean.database.DatabaseConnector;
 
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(App.class, args);
 
-        MysqlConnector mysqlConnector = context.getBean(MysqlConnector.class);
+        DatabaseConnector mysqlConnector = context.getBean(MysqlConnector.class);
         System.out.println(mysqlConnector.connect());
     }
 }
--- src/main/java/com/dzung/springboot/configurationbean/AppConfig.java
+++ src/main/java/com/dzung/springboot/configurationbean/AppConfig.java
@@ -2,18 +2,28 @@ package com.dzung.springboot.configurationbean;
 
 import com.dzung.springboot.configurationbean.database.DatabaseConnector;
 import com.dzung.springboot.configurationbean.database.MysqlConnector;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
 
+    @Value("${spring.datasource.password}")
+    String password;
+
+    @Value("${spring.datasource.username}")
+    String userName;
+
+    @Value("${spring.datasource.url}")
+    String url;
+
     @Bean
     DatabaseConnector getConnection() {
         DatabaseConnector mysqlConnector = new MysqlConnector();
-        mysqlConnector.setUrl("jdbc:mysql://localhost:3306/learning");
-        mysqlConnector.setUserName("root");
-        mysqlConnector.setPassword("Dung3032003_135709");
+        mysqlConnector.setUrl(url);
+        mysqlConnector.setUserName(userName);
+        mysqlConnector.setPassword(password);
 
         return mysqlConnector;
     }
