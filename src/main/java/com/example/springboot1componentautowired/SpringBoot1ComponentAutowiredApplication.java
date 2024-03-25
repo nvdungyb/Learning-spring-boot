16f2673f4e6c769c0733b9d264aa555816e9d0c9
NvDzung <dung3032003md@gmail.com>
2024-03-25 20:56:06 +0700

    Component Autowired

---
+++ src/main/java/com/example/springboot1componentautowired/SpringBoot1ComponentAutowiredApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot1componentautowired;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot1ComponentAutowiredApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot1ComponentAutowiredApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springboot1componentautowired/SpringBoot1ComponentAutowiredApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot1componentautowired;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot1ComponentAutowiredApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
