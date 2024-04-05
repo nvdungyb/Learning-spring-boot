763c9135bf472b7d340ee5533d11bde0a9a275b9
NvDzung <dung3032003md@gmail.com>
2024-04-05 22:54:57 +0700

    @ConfigurationProperties

--- src/main/java/com/dzungyb/learning_spring_boot/LearningSpringBootApplication.java
+++ src/main/java/com/dzungyb/learning_spring_boot/LearningSpringBootApplication.java
@@ -1,15 +1,35 @@
 package com.dzungyb.learning_spring_boot;
 
+import com.dzungyb.learning_spring_boot.configuration.Properties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
-public class LearningSpringBootApplication {
+public class LearningSpringBootApplication implements CommandLineRunner {
+    @Autowired
+    private Properties properties;
 
     public static void main(String[] args) {
         SpringApplication.run(LearningSpringBootApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Email: " + properties.getEmail());
+        System.out.println("Password: " + properties.getPassword());
+        System.out.println("username: " + properties.getUsername());
+    }
 }
+
+/*  implements CommanlineRunner
+1) Khởi tạo ứng dụng: SpringBoot sẽ bắt đầu bằng việc khởi tạo các thành phần cần thiết, cấu hình các bean và context ứng dung.
+2) Tự động gọi phương thức run: Sau khi context của ứng dụng đã được khởi tạo, SpringBoot sẽ tự động gọi phương thức run của bean được chứa trong context.
+3) Thực thi logic trong phương thức run: các logic trong phương thức run sẽ được thực thi. Cho phép thực hiện các công việc cần thiết trước khi ứng dụng bắt đầu chạy chính thức.
+như khởi tạo dữ liệu, cấu hình các thông số, kết nối với các service khác, ...
+4) Chạy ứng dụng: Sau khi phương thức run đã được thực thi, ứng dụng sẽ bắt đầu chạy chính thức.
+
+ */
--- src/main/java/com/dzungyb/learning_spring_boot/configuration/Properties.java
+++ src/main/java/com/dzungyb/learning_spring_boot/configuration/Properties.java
@@ -1,2 +1,14 @@
-package com.dzungyb.learning_spring_boot.configuration;public class Properties {
+package com.dzungyb.learning_spring_boot.configuration;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component                                  // Sử dụng để Spring biết đây là một Bean và khởi tạo nó.
+@ConfigurationProperties(prefix = "app")    // Sử dụng để map các properties trong file application.properties vào class này.
+public class Properties {
+    private String email;
+    private String password;
+    private String username;
 }
